bf6432bf28663d71ed9d380d2758aa965402690f
matemato <47794629+matemato@users.noreply.github.com>
2022-03-27 03:14:49 +0200

    Almost finished

--- Assignments/2/ms_tracker.py
+++ Assignments/2/ms_tracker.py
@@ -18,33 +18,71 @@ class MSTracker(Tracker):
         region[3] = int(region[3])
         if region[2]%2==0: region[2] -= 1
         if region[3]%2==0: region[3] -= 1
-        self.size = (region[2], region[3])
+        self.size = [region[2], region[3]]
         self.X, self.Y = np.meshgrid(np.arange(-int(self.size[0]/2), int(self.size[0]/2) + 1), np.arange(-int(self.size[1]/2), int(self.size[1]/2) + 1))
-        self.ep = create_epanechnik_kernel(self.size[0], self.size[1], 1)
+        self.kernel = create_epanechnik_kernel(self.size[0], self.size[1], self.parameters.sigma)
         patch,_ = get_patch(image, self.position, self.size)
-        self.q = extract_histogram(patch, 16, self.ep)
-        self.q = self.q/np.sum(self.q)
+        self.q = extract_histogram(patch, self.parameters.bins, self.kernel)
 
-        plt.imshow(image)
-        plt.plot(self.position[0], self.position[1], 'rx')
-        plt.show()
+        self.c = 1
+        # self.c = self.get_background_info(image)
+        
+        self.q = self.c * self.q
+        self.q = self.q/np.sum(self.q)
+        # plt.imshow(image)
+        # plt.plot(self.position[0], self.position[1], 'rx')
+        # plt.show()
 
 
     def track(self, image):
         for i in range(20):
             patch,_ = get_patch(image, self.position, self.size)
-            p = extract_histogram(patch, 16, self.ep)
+
+            # plt.imshow(patch)
+            # plt.plot(self.position[0], self.position[1], 'rx')
+            # plt.show()
+
+            p = extract_histogram(patch, self.parameters.bins, self.kernel) * self.c
             p = p/np.sum(p)
 
-            backproject = backproject_histogram(patch, np.sqrt(self.q/(p+1e-3)),16)
+            backproject = backproject_histogram(patch, np.sqrt(self.q/(p+self.parameters.eps)), self.parameters.bins)
             backproject = backproject/np.sum(backproject)
 
-            self.position[0] = self.position[0] + np.sum(self.X*backproject)
-            self.position[1] = self.position[1] + np.sum(self.Y*backproject)
+            x_shift = np.sum(self.X*backproject)
+            y_shift = np.sum(self.Y*backproject)
+            self.position[0] += x_shift
+            self.position[1] += y_shift
+
+            if int(np.sqrt(x_shift**2 + y_shift**2)) <= self.parameters.stop_dist:
+                break
+
+        # plt.imshow(patch)
+        # plt.plot(self.position[0], self.position[1], 'rx')
+        # plt.show()
+        
+        self.update_q(patch)
+        
 
         return [self.position[0] - self.size[0]/2, self.position[1] - self.size[1]/2, self.size[0], self.size[1]]
 
+    def get_background_info(self, image):
+        background_patch,_ = get_patch(image, self.position, np.array(self.size)*3)
+        background_hist = extract_histogram(background_patch, self.parameters.bins, create_epanechnik_kernel(self.size[0]*3, self.size[1]*3, self.parameters.sigma)) - self.q
+
+        o_min = min(background_hist[background_hist>0])
+        background_hist = [max(i, 0) for i in background_hist]
+        return [min(o_min/i, 1) for i in background_hist]
+
+    def update_q(self, patch):
+        new_q = extract_histogram(patch, self.parameters.bins, self.kernel) * self.c
+        new_q = new_q/np.sum(new_q)
+        self.q = (1-self.parameters.alpha) * self.q + self.parameters.alpha * new_q
+
 
 class MSParams():
-    def __init__(self):
-        self.enlarge_factor = 2
+    def __init__(self, p):
+        self.stop_dist = 1
+        self.bins = 16
+        self.eps = 1e-5
+        self.sigma = 0.5
+        self.alpha = 0.05
--- Assignments/2/run_vaja_2.py
+++ Assignments/2/run_vaja_2.py
@@ -4,23 +4,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ex2_utils import generate_responses_1, create_epanechnik_kernel, get_patch, extract_histogram
 
-def test():
-    im = cv2.imread("./Sequences/ball/00000000.jpg")
-    start = [222,134]
-    size = [45, 45]
+def mean_shift():
+    pdf = generate_responses_1()
+    start = [80, 40]
+    real_start = [80, 40]
+
+    size = [31, 31]
     [X, Y] = np.meshgrid(np.arange(-int(size[0]/2), int(size[0]/2) + 1), np.arange(-int(size[1]/2), int(size[1]/2) + 1))
-    wi,_ = get_patch(im, start, size)
-    
-    h = extract_histogram(wi, 16)
-    plt.hist(h)
-    plt.show()
 
-    plt.imshow(wi)
-    plt.show()
+    filtered = generate_responses_1()
+    for i in range(20):
+        w,_ = get_patch(filtered, start, size)
+        w = w/np.sum(w)
+        x_shift = np.sum(np.multiply(w, X))
+        y_shift = np.sum(np.multiply(w, Y))
+        start[0] += x_shift
+        start[1] += + y_shift
 
+        if np.sqrt(x_shift**2 + y_shift**2) < 0.5:
+            print(i)
+            break
 
+    plt.imshow(filtered)
+    plt.plot(start[0],start[1], 'rx')
+    plt.plot(real_start[0],real_start[1], 'ro')
+    plt.show() 
 
 if __name__ == "__main__":
-    test()
+    mean_shift()
 
 
